c73278dc94cfebc6b1360a8e3cecdaec195afe56
Jay <patej13@farmingdale.edu>
2024-11-25 17:10:32 -0500

    Added Splash Screen to the app, it should display a temporary logo as the app is opened

---
+++ app/src/main/java/com/example/trashthrashersapp/SplashScreen.kt
@@ -0,0 +1,42 @@
+package com.example.trashthrashersapp
+
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.NavHostController
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(navController: NavHostController) {
+
+    val scale= remember {
+        Animatable(0f, 1f)
+    }
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(
+            targetValue = 0.5f,
+            animationSpec = tween(durationMillis = 1000,0, easing = {
+                OvershootInterpolator(2f).getInterpolation(it)
+            }
+            ))
+        delay(3000)
+        navController.navigate(NavigationItems.ProfileScreen.route)
+    }
+
+    Column (modifier = Modifier.fillMaxSize().wrapContentSize(Alignment.Center)){
+        Image(painter = painterResource(id = R.drawable.templogo), contentDescription ="" )
+    }
+}
