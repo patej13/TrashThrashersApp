d065e4a8472831b339d728a924e33ff0db84a800
Jay <patej13@farmingdale.edu>
2024-11-30 17:56:04 -0500

    Added temp code for sign up, matches the login

--- app/src/main/java/com/example/trashthrashersapp/SignupViewModel.kt
+++ app/src/main/java/com/example/trashthrashersapp/SignupViewModel.kt
@@ -1,4 +1,18 @@
 package com.example.trashthrashersapp
 
-class SignupViewModel {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class SignUpViewModel : ViewModel(){
+    var email by mutableStateOf("")
+    var password by mutableStateOf("")
+
+    fun onEmailChange(newEmail: String) {
+        email = newEmail
+    }
+    fun onPasswordChange(newPassword: String) {
+        password = newPassword
+    }
 }
