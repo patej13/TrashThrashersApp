460ecff86c1add41eecf427ab5f3072b88bdc6b4
Jay <patej13@farmingdale.edu>
2024-12-02 20:51:31 -0500

    Added firestore dependencies to build gradle and libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.common.ktx)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.firebase.firestore.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
