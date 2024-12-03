eae9fcc225f7016454e11654b01ca573269ef465
Jay <patej13@farmingdale.edu>
2024-12-02 21:01:15 -0500

    The username is now being displayed in the profile screen.

--- app/src/main/java/com/example/trashthrashersapp/ProfileScreen.kt
+++ app/src/main/java/com/example/trashthrashersapp/ProfileScreen.kt
@@ -6,9 +6,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 /**
  * This is where the profile info goes, just contains text at the moment
@@ -18,15 +26,27 @@ fun ProfileScreen(
     navController: NavHostController,
     modifier: Modifier = Modifier,
 ) {
+    val user = Firebase.auth.currentUser
+    var userName by remember { mutableStateOf("") }
+
+    LaunchedEffect(user?.uid) {
+        val firestoreDB = FirebaseFirestore.getInstance()
+        val userInformation = user?.let { firestoreDB.collection("Info").document(it.uid) }
+        userInformation?.get()?.addOnSuccessListener { document ->
+            userName = document.getString("username").toString()
+        }
+    }
     Column(
         modifier = modifier.padding(10.dp)
     ) {
         Text(
             "Temporary Profile Screen"
         )
-        Text(
-            "Username"
-        )
+        if (user != null) {
+            Text("Hello $userName")
+        } else {
+            Text("")
+        }
         Button(
             onClick = {
                 navController.navigate(NavigationItems.Home.route)
