d9b676778eff08980fd9ab654e960a8f652e49af
ianazimov <ianazimov@gmail.com>
2024-12-04 14:24:08 -0500

    Start from scratch, just setting up camera composable

--- app/src/main/java/com/example/trashthrashersapp/CameraScreen.kt
+++ app/src/main/java/com/example/trashthrashersapp/CameraScreen.kt
@@ -1,42 +1,14 @@
 package com.example.trashthrashersapp
 
-
-import android.graphics.Camera
-import android.widget.Button
-import android.widget.ImageView
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.google.android.gms.auth.api.phone.SmsCodeAutofillClient.PermissionState
 
-
-/**
- * This is where camera code will go
- */
-
-/*
 @Composable
 fun CameraScreen(){
 
-
-
 }
-*/
 
 
-@Composable
-fun CameraScreen(
-    modifier: Modifier = Modifier,
-) {
-    Column(
-        modifier = modifier.padding(10.dp)
-    ) {
-        Text(
-            "Temporary Camera Screen"
-        )
-    }
-}
+
+
+
 
