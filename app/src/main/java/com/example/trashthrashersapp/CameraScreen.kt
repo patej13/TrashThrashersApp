4dd8fbadc9096a8af290e853bee726176747ec7b
ianazimov <ianazimov@gmail.com>
2024-12-04 14:34:56 -0500

    added area for image to be displayed, now gonna work on button

--- app/src/main/java/com/example/trashthrashersapp/CameraScreen.kt
+++ app/src/main/java/com/example/trashthrashersapp/CameraScreen.kt
@@ -5,9 +5,14 @@ import android.graphics.ImageDecoder
 import android.net.Uri
 import android.os.Build
 import android.provider.MediaStore
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,7 +20,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun CameraScreen(){
@@ -39,7 +46,17 @@ fun CameraScreen(){
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ){
+        bitmap.value?.let { btm ->
+            Image(
+                bitmap = btm.asImageBitmap(),
+                contentDescription = "Picked Image",
+                modifier = Modifier
+                    .size(400.dp)
+                    .padding(20.dp)
+            )
+        }
 
+        Spacer(modifier = Modifier.height(12.dp))
     }
 }
 
