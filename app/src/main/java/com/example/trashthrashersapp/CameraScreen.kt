b666a1fca70609d02ecb417c6003d92586487428
ianazimov <ianazimov@gmail.com>
2024-12-04 14:27:48 -0500

    variables I will need, defined those

--- app/src/main/java/com/example/trashthrashersapp/CameraScreen.kt
+++ app/src/main/java/com/example/trashthrashersapp/CameraScreen.kt
@@ -1,9 +1,21 @@
 package com.example.trashthrashersapp
 
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
 
 @Composable
 fun CameraScreen(){
+    var imageUri by remember { mutableStateOf<Uri?>(null) }
+    var uploadedImageUrl by remember { mutableStateOf<String?>(null) }
+    val context = LocalContext.current
+    val bitmap = remember { mutableStateOf<Bitmap?>(null) }
+
 
 }
 
