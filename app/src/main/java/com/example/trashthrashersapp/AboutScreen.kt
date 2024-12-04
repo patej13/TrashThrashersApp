13414d9d4ac926f34c8c2477ef14819e538adcfe
pellma <mikeythechemist@gmail.com>
2024-12-04 16:40:49 -0500

    Adjusted the AboutScreen page

--- app/src/main/java/com/example/trashthrashersapp/AboutScreen.kt
+++ app/src/main/java/com/example/trashthrashersapp/AboutScreen.kt
@@ -1,10 +1,15 @@
 package com.example.trashthrashersapp
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -14,8 +19,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.trashthrashersapp.ui.theme.CustomOrange
+import com.example.trashthrashersapp.ui.theme.NeutralBackground
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
@@ -30,8 +39,8 @@ fun AboutScreen(
     val firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
     val firestoreDB = FirebaseFirestore.getInstance()
     var userName by remember { mutableStateOf("") }
-    var email by  remember { mutableStateOf("") }
-    var password by  remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
     var trashCollected by remember { mutableStateOf("") }
     var trashMarked by remember { mutableStateOf("") }
 
@@ -41,62 +50,97 @@ fun AboutScreen(
         userInformation?.get()?.addOnSuccessListener { document ->
             userName = document.getString("username").toString()
             email = document.getString("email").toString()
-            password =  document.getString("password").toString()
+            password = document.getString("password").toString()
             trashCollected = document.getString("trashCollected").toString()
             trashMarked = document.getString("trashMarked").toString()
         }
     }
-    Column(
-        modifier = modifier.padding(10.dp), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Bottom
-    ) {
-        Text(
-            "Random"
-        )
-        Button(
-            onClick = {
-                var trashMarkedInt = trashMarked.toInt()
-                trashMarkedInt++
-                trashMarked = trashMarkedInt.toString()
-                val user = firebaseAuth.currentUser
-                user?.let {
-                    val userInfo = firestoreDB.collection("Info").document(it.uid)
-                    val userData = hashMapOf(
-                        "email" to email,
-                        "password" to password,
-                        "username" to userName,
-                        "trashMarked" to trashMarked,
-                        "trashCollected" to trashCollected,
-                        "uid" to it.uid
-                    )
-                    userInfo.set(userData)
-                }
-            },
-            modifier = Modifier.fillMaxWidth()
+
+    TealBackground {
+        Column(
+            modifier = modifier.padding(10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Bottom
         ) {
-            Text("Add Trash Marked")
-        }
-        Button(
-            onClick = {
-                var trashCollectedInt = trashCollected.toInt()
-                trashCollectedInt++
-                trashCollected = trashCollectedInt.toString()
-                val user = firebaseAuth.currentUser
-                user?.let {
-                    val userInfo = firestoreDB.collection("Info").document(it.uid)
-                    val userData = hashMapOf(
-                        "email" to email,
-                        "password" to password,
-                        "username" to userName,
-                        "trashMarked" to trashMarked,
-                        "trashCollected" to trashCollected,
-                        "uid" to it.uid
+            Column(
+                modifier = Modifier
+                    .padding(top = 40.dp)
+                    .fillMaxWidth()
+                    .background(
+                        color = NeutralBackground,
+                        shape = androidx.compose.foundation.shape.RoundedCornerShape(16.dp)
                     )
-                    userInfo.set(userData)
+                    .padding(vertical = 16.dp, horizontal = 16.dp)
+            ) {
+
+                Text(
+                    fontSize = 24.sp,
+                    text = "About Our App",
+                    modifier = Modifier.padding(bottom = 20.dp)
+                )
+                Text(
+                    fontSize = 16.sp,
+                    text = "The purpose of this app is to encourage trash collection on the FSC campus. This goal will be achieved by marking locations that contain litter or other trash on a map. Users will also take a picture of the trash, this picture will be uploaded. The amount of locations marked or collected will be visible in the profile tab, as well as the leaderboard tab."
+                )
+                Spacer(modifier = Modifier.height(60.dp))
+                Button(
+                    onClick = {
+                        var trashMarkedInt = trashMarked.toInt()
+                        trashMarkedInt++
+                        trashMarked = trashMarkedInt.toString()
+                        val user = firebaseAuth.currentUser
+                        user?.let {
+                            val userInfo = firestoreDB.collection("Info").document(it.uid)
+                            val userData = hashMapOf(
+                                "email" to email,
+                                "password" to password,
+                                "username" to userName,
+                                "trashMarked" to trashMarked,
+                                "trashCollected" to trashCollected,
+                                "uid" to it.uid
+                            )
+                            userInfo.set(userData)
+                        }
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = CustomOrange,
+                        contentColor = Color.White),
+                    shape = RoundedCornerShape(30.dp),
+                    modifier = Modifier.height(60.dp)
+                        .fillMaxWidth(),
+                ) {
+                    Text("Add Trash Marked")
                 }
-            },
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text("Add Trash Collected")
+                Spacer(modifier=Modifier.height(15.dp))
+
+                Button(
+                    onClick = {
+                        var trashCollectedInt = trashCollected.toInt()
+                        trashCollectedInt++
+                        trashCollected = trashCollectedInt.toString()
+                        val user = firebaseAuth.currentUser
+                        user?.let {
+                            val userInfo = firestoreDB.collection("Info").document(it.uid)
+                            val userData = hashMapOf(
+                                "email" to email,
+                                "password" to password,
+                                "username" to userName,
+                                "trashMarked" to trashMarked,
+                                "trashCollected" to trashCollected,
+                                "uid" to it.uid
+                            )
+                            userInfo.set(userData)
+                        }
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = CustomOrange,
+                        contentColor = Color.White),
+                    shape = RoundedCornerShape(30.dp),
+                    modifier = Modifier.height(60.dp)
+                        .fillMaxWidth(),                ) {
+                    Text("Add Trash Collected")
+                }
+            }
         }
     }
 }
