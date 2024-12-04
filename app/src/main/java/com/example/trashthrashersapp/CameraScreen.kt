215295dcb13d1d17612192a1550d29e2b558f4d3
ianazimov <ianazimov@gmail.com>
2024-12-04 14:30:41 -0500

    convert Uri to bitmap so we can display picked image

--- app/src/main/java/com/example/trashthrashersapp/CameraScreen.kt
+++ app/src/main/java/com/example/trashthrashersapp/CameraScreen.kt
@@ -1,7 +1,10 @@
 package com.example.trashthrashersapp
 
 import android.graphics.Bitmap
+import android.graphics.ImageDecoder
 import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -17,6 +20,14 @@ fun CameraScreen(){
     val bitmap = remember { mutableStateOf<Bitmap?>(null) }
 
 
+    imageUri?.let {
+        if (Build.VERSION.SDK_INT < 28) {
+            bitmap.value = MediaStore.Images.Media.getBitmap(context.contentResolver, it)
+        } else {
+            val source = ImageDecoder.createSource(context.contentResolver, it)
+            bitmap.value = ImageDecoder.decodeBitmap(source)
+        }
+    }
 }
 
 
